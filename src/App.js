f6933c5d17c6bf2716524b516b9a7308d72d87a0
VIGNESH M <vigneshm5142@gmail.com>
2022-12-07 16:12:12 +0530

    weather

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+const api = {
+  key: "72d690760e3f83721522b446f4520434",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const searchPressed = () => {
+    fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setWeather(result);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>FIND WEATHER</h1>
+        {/* //***************** search box and button *********************************  */}
+        
+        <div>
+          <input
+          className="input"
+            type="text"
+            placeholder="Please Enter city/town..."
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button 
+          onClick={searchPressed}
+          className="search-button"
+          >Search</button>
+        </div>
+
+        {/* //***************** weather details *********************************  */}
+
+        {typeof weather.main !== "undefined" ? (
+          <div>
+            <p>{weather.name}</p>
+            <p>{weather.main.temp}°C</p>
+            <p>{weather.weather[0].main}</p>
+            <p>({weather.weather[0].description})</p>
+          </div>
+        ) : (
+          ""
+        )}
       </header>
     </div>
   );
